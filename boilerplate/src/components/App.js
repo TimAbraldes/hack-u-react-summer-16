97ab36016df9cf3437d2a4cffc42d1e591d642e2
Tim Falletta Abraldes <timabraldes@gmail.com>
2016-08-01 19:10:21 -0700

    A small exercise - change colors on button press

--- boilerplate/src/components/App.js
+++ boilerplate/src/components/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
+const colors = [
+  '#FF0000',
+  '#00FF00',
+  '#0000FF',
+  '#FF00FF',
+  '#00FFFF',
+];
+
 export default class App extends Component {
+  state = {
+    currentColorIndex: 0,
+  };
+
+  nextColor() {
+    this.setState({
+      currentColorIndex: (this.state.currentColorIndex + 1) % colors.length,
+    });
+  }
+
   render() {
     return (
       <div className='container'>
-        <h1>Hello, world!</h1>
-        <p>Let's get funky.</p>
+        <div style={ { width: 200, height: 100, backgroundColor: colors[this.state.currentColorIndex] } }></div>
+        <button onClick={this.nextColor.bind(this)}>Next color</button>
       </div>
     );
   }
