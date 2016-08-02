276fa1bf60e38b887a1de8ab3591f2e34c13b2b9
Tim Falletta Abraldes <timabraldes@gmail.com>
2016-08-01 19:51:11 -0700

    Working counter

--- boilerplate/src/components/App.js
+++ boilerplate/src/components/App.js
@@ -4,8 +4,31 @@ export default class App extends Component {
   render() {
     return (
       <div className='container'>
-        <h1>Hello, world!</h1>
-        <p>Let's get funky.</p>
+        <CountBy by={1} />
+        <CountBy by={2} />
+        <CountBy by={3} />
+        <CountBy by={4} />
+      </div>
+    );
+  }
+}
+
+class CountBy extends Component {
+  state = {
+    count: 0,
+  };
+
+  inc() {
+    this.setState({
+      count: this.state.count + this.props.by,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type='number' value={this.state.count} />
+        <button onClick={this.inc.bind(this)}>Inc by {this.props.by}</button>
       </div>
     );
   }
